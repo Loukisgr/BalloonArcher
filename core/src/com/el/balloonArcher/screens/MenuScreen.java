fd5a83c307e9390a048e01d42978720cd5469dea
liaras <loukisgr@gmail.com>
2016-10-07 17:39:13 +0300

    Menu Screen Backgrounds

--- core/src/com/el/balloonArcher/screens/MenuScreen.java
+++ core/src/com/el/balloonArcher/screens/MenuScreen.java
@@ -3,7 +3,10 @@ package com.el.balloonArcher.screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
@@ -14,6 +17,9 @@ import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.el.balloonArcher.util.Constants;
 
 /**
@@ -42,7 +48,7 @@ public class MenuScreen extends AbstractGameScreen
     private Slider sldSound;
     private CheckBox chkMusic;
     private Slider sldMusic;
-    private SelectBox<CharacterSkin> selCharSkin;
+   // private SelectBox<CharacterSkin> selCharSkin;
     private Image imgCharSkin;
     private CheckBox chkShowFpsCounter;
     // debug
@@ -61,23 +67,62 @@ public class MenuScreen extends AbstractGameScreen
         Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        if(Gdx.input.isTouched())
+  /*      if(Gdx.input.isTouched())
         {
             game.setScreen(new GameScreen(game));
         }
+*/
+        if (debugEnabled)
+        {
+            debugRebuildStage -= deltaTime;
+            if (debugRebuildStage <= 0)
+            {
+                debugRebuildStage = DEBUG_REBUILD_INTERVAL;
+                rebuildStage();
+            }
+        }
+
+        stage.act(deltaTime);
+        stage.draw();
+        //Table.drawDebug(stage);
     }
 
-    @Override public void resize (int width, int height) { }
-    @Override public void show () { }
-    @Override public void hide () { }
+    @Override public void resize (int width, int height)
+    {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override public void show ()
+    {
+        stage = new Stage(new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        Gdx.input.setInputProcessor(stage);
+        rebuildStage();
+    }
+
+    @Override public void hide ()
+    {
+        stage.dispose();
+        skinBalloonArcher.dispose();
+    }
+
     @Override public void pause () { }
 
     private void rebuildStage ()
     {
 
-        skinBalloonArcher = new Skin(
+      /*  skinBalloonArcher = new Skin(
                 Gdx.files.internal(Constants.SKIN_BALLOONARCHER_UI),
-                new TextureAtlas(Constants.TEXTURE_ATLAS_UI));
+                new TextureAtlas(Constants.TEXTURE_ATLAS_UI));*/
+
+        skinBalloonArcher = new Skin();
+        skinBalloonArcher.add("logo",new TextureRegion(new Texture("images/main_screen_background.png"),258,0,100,47));
+        skinBalloonArcher.add("balloons",new TextureRegion(new Texture("images/main_screen_background.png"),188,71,135,61));
+        skinBalloonArcher.add("archer",new TextureRegion(new Texture("images/main_screen_background.png"),0,0,148,203));
+        skinBalloonArcher.add("info",new TextureRegion(new Texture("images/main_screen_background.png"),394,483,118,29));
+        skinBalloonArcher.add("background",new TextureRegion(new Texture("images/main_screen_background.png"),500,0,12,12));
+        skinBalloonArcher.add("play",new TextureRegion(new Texture("images/main_screen_background.png"),463,393,49,28));
+        skinBalloonArcher.add("options",new TextureRegion(new Texture("images/main_screen_background.png"),463,500,49,28));
+
 // build all layers
         Table layerBackground = buildBackgroundLayer();
         Table layerObjects = buildObjectsLayer();
@@ -99,30 +144,91 @@ public class MenuScreen extends AbstractGameScreen
     private Table buildBackgroundLayer ()
     {
         Table layer = new Table();
+        this.imgBackground = new Image(skinBalloonArcher.getRegion("background"));
         return layer;
     }
 
     private Table buildObjectsLayer ()
     {
         Table layer = new Table();
+        imgBalloons = new Image(skinBalloonArcher.getRegion("balloons"));
+        layer.addActor(imgBalloons);
+        imgBalloons.setPosition(135, 80);
+
+        imgArcher = new Image(skinBalloonArcher.getRegion("archer"));
+        layer.addActor(imgArcher);
+        imgArcher.setPosition(355, 40);
         return layer;
     }
 
     private Table buildLogosLayer ()
     {
         Table layer = new Table();
+        imgLogo = new Image(skinBalloonArcher.getRegion("logo"));
+        layer.addActor(imgLogo);
+        layer.row().expandY();
+
+        imgInfo = new Image(skinBalloonArcher.getRegion("info"));
+        layer.add(imgInfo).bottom();
+
+        if (debugEnabled)
+        {
+            layer.debug();
+        }
+
         return layer;
     }
 
     private Table buildControlsLayer ()
     {
         Table layer = new Table();
+        // + Play Button
+        Image imgMenuPlay = new Image(skinBalloonArcher.getRegion("play"));
+        btnMenuPlay = new Button(imgMenuPlay.getDrawable());
+
+        layer.add(btnMenuPlay);
+
+        btnMenuPlay.addListener(new ChangeListener()
+        {
+            @Override
+            public void changed (ChangeEvent event, Actor actor)
+            {
+                onPlayClicked();
+            }
+        });
+
+        layer.row();
+
+// + Options Button
+        Image imgMenuoptions = new Image(skinBalloonArcher.getRegion("options"));
+        btnMenuOptions = new Button(imgMenuoptions.getDrawable());
+
+        layer.add(btnMenuOptions);
+        btnMenuOptions.addListener(new ChangeListener()
+        {
+            @Override
+            public void changed (ChangeEvent event, Actor actor)
+            {
+                onOptionsClicked();
+            }
+        });
+
+        if (debugEnabled) layer.debug();
+
         return layer;
     }
 
     private Table buildOptionsWindowLayer ()
     {
         Table layer = new Table();
+        layer.right().bottom();
         return layer;
     }
+
+    private void onPlayClicked ()
+    {
+        game.setScreen(new GameScreen(game));
+    }
+
+    private void onOptionsClicked () { }
 }
